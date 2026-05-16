14cd5a08ba069c5a88d31f9a70887516ba390072
yashpratapkushwaha966 <yashkushwaha485005@gmail.com>
2026-05-16 22:57:23 +0530

    remove nodemailer

--- Portfolio_backend/server/routes/contactRoutes.js
+++ Portfolio_backend/server/routes/contactRoutes.js
@@ -6,14 +6,17 @@ const Contact = require('../models/contact')
 router.post('/', async (req, res) => {
   try {
     const newMessage = new Contact(req.body)
+
     await newMessage.save()
 
     res.status(201).json({
       success: true,
       message: 'Message Saved Successfully',
     })
+
   } catch (error) {
     console.log(error)
+
     res.status(500).json({
       success: false,
       message: 'Server Error',
@@ -21,4 +24,4 @@ router.post('/', async (req, res) => {
   }
 })
 
-module.exports = router
+module.exports = router 
